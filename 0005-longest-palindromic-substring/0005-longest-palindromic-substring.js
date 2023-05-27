8aaab45be8cbd05b8ee1c3761674735ec86adc66
EJKim3191 <95759697+EJKim3191@users.noreply.github.com>
2023-05-27 21:42:25 +0900

    Time: 506 ms (35.07%), Space: 49 MB (26.65%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+let isPalindrome = function(s){
+    let isTrue = true;
+    let front = 0;
+    let back = s.length - 1;
+    while(true){
+        if(front > back) break;
+        if(s[front] !== s[back]){
+            isTrue = false;
+            break;
+        }
+        front += 1;
+        back -= 1;
+    }
+    return isTrue;
+}
+var longestPalindrome = function(s) {
+    //1st try - from longest, search for palindrome
+    let length = s.length;
+    let answer = '';
+    for(let i=0; i<=length; i+=1){
+
+        for(let j=0; j<=i; j+=1){
+            let front = 0;
+            let back = length - i;
+            front += j;
+            back += j;
+            let palindrome = s.slice(front, back);
+            // console.log(palindrome)
+            if(isPalindrome(palindrome)){
+                answer = palindrome;
+                break;
+            } 
+        }
+        if(answer !== '') break;
+    }
+    return answer;
+};
